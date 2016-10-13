b584fb5920f6f4cb875e9e0e89a17a4b3b209baf
Brais <Brais@Brais-PC>
2016-10-13 09:13:50 +0200

    Primer commit solapamiento

---
+++ src/ejerciciosolapamiento/Ejerciciosolapamiento.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosolapamiento;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ejerciciosolapamiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        x = Integer.parseInt(JOptionPane.showInputDialog("Introducir valor de x"));
+       
+        // TODO code application logic here
+    }
+    
+}
